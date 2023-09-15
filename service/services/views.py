bd49fbf47e825e994139a83c91c8d70f46f75f1a
Sergey Emashev <SergeyEmashev@gmail.com>
2023-09-15 13:52:13 +0400

    Day 9 - 1

--- service/services/views.py
+++ service/services/views.py
@@ -1,14 +1,17 @@
+from django.conf import settings
+from django.core.cache import cache
 from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
-from services.models import Subscription
 from services.serializers import SubscriptionSerializer
+from services.models import Subscription
 
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
+
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client',
@@ -22,8 +25,16 @@ class SubscriptionView(ReadOnlyModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
 
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
+
         response_data = {'result': response.data}
-        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total_amount'] = total_price
         response.data = response_data
 
         return response
